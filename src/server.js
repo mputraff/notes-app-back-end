e200cb82445e5380e7d6ecf49aa2f56a7cb5f0bd
putrafauzan32@gmail.com <email@example.com>
2024-11-05 21:51:52 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ import routes from "./routes.js";
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: "localhost",
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
   });
 
   server.route(routes);
